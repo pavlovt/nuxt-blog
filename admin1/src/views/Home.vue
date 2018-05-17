86a1eb021a04996f489a6c332fc500e78a32a8de
Todor Pavlov <pavlovt@gmail.com>
2018-05-17 22:42:21 +0300

    add webpack 4 and easy-html

---
+++ admin1/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template lang="ehtml">
+  div.row {
+      div.col {
+          form :submit=submit() {
+              input type=number {}
+
+              button.btn.btn-primary { 'Submit' }
+          }
+      }
+  }
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+// const t = require('./t.ehtml')
+// console.log('ttt', t)
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    submit() {}
+  }
+}
+</script>
