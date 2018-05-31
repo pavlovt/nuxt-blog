6a13e038f1656608a4c1d870c692c5222274c5e4
Todor Pavlov <pavlovt@gmail.com>
2018-05-31 09:26:15 +0300

    add example filter

--- admin1/src/core/core.plugin.js
+++ admin1/src/core/core.plugin.js
@@ -7,5 +7,11 @@ export default {
         console.log('Mounted!', this);
       }
     })*/
+
+    Vue.filter('capitalize', function (value) {
+		  if (!value) return ''
+		  value = value.toString()
+		  return value.charAt(0).toUpperCase() + value.slice(1)
+		})
   }
 }
